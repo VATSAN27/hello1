a88514fcbe51c2629a6392b3353a6cc611718d40
VATSAN27 <157441061+VATSAN27@users.noreply.github.com>
2024-02-04 13:36:26 +0530

    Add files via upload

---
+++ posOrNeg.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n=-17;
+        if(n>0)
+        {
+            System.out.println("it is positive");
+        }
+        else if(n<0)
+        {
+            System.out.println("it is negative");
+        }
+    }
+}
